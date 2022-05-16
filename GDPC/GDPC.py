2d86d95afb4002fb22789b68d647a102b9280ba1
Hakimeh B. Khojasteh <72028345+Khojasteh-hb@users.noreply.github.com>
2022-05-16 23:19:02 +0430

    Add files via upload

---
+++ GDPC/GDPC.py
@@ -0,0 +1,59 @@
+
+import re
+import readFasta
+import numpy as np
+import pandas as pd
+
+def GDPC(fastas, **kw):
+	group = {
+		'alphaticr': 'GAVLMI',
+		'aromatic': 'FYW',
+		'postivecharger': 'KRH',
+		'negativecharger': 'DE',
+		'uncharger': 'STCPNQ'
+	}
+
+	groupKey = group.keys()
+	baseNum = len(groupKey)
+	dipeptide = [g1+'.'+g2 for g1 in groupKey for g2 in groupKey]
+
+	index = {}
+	for key in groupKey:
+		for aa in group[key]:
+			index[aa] = key
+
+	encodings = []
+	header = ['#'] + dipeptide
+	encodings.append(header)
+
+	for i in fastas:
+		name, sequence = i[0], re.sub('-', '', i[1])
+
+		code = [name]
+		myDict = {}
+		for t in dipeptide:
+			myDict[t] = 0
+
+		sum = 0
+		for j in range(len(sequence) - 2 + 1):
+			myDict[index[sequence[j]]+'.'+index[sequence[j+1]]] = myDict[index[sequence[j]]+'.'+index[sequence[j+1]]] + 1
+			sum = sum +1
+
+		if sum == 0:
+			for t in dipeptide:
+				code.append(0)
+		else:
+			for t in dipeptide:
+				code.append(myDict[t]/sum)
+		encodings.append(code)
+
+	return encodings
+
+kw=  {'path': r"Enzyme.txt",'order': 'ACDEFGHIKLMNPQRSTVWY'}
+fastas1 = readFasta.readFasta(r"Enzyme.txt")
+
+result = GDPC(fastas1, **kw)
+
+data1 = np.matrix(result[1:])[:,1:]
+data_ = pd.DataFrame(data=data1)
+data_.to_csv('GDPC_Enzyme.csv')
